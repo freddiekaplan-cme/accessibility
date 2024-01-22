4ff01effd6a280c6c5f3a70f54a45cb63b8220f9
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2024-01-22 21:28:09 +0100

    Add mobile menu functionality to header.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,43 +1,77 @@
+"use client"
+import React, { useEffect, useState } from "react"
 import Link from "next/link"
-import React from "react"
 
 const Header = () => {
+	const [showMenu, setShowMenu] = useState<boolean>(false)
+
+	const handleClick = () => {
+		setShowMenu(!showMenu)
+	}
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 640) {
+				setShowMenu(false)
+			}
+		}
+
+		window.addEventListener("resize", handleResize)
+
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
+
 	return (
-		<header className="flex flex-col p-4 sm:p-8 justify-center">
-			<h1 className="flex flex-row font-bold text-3xl">
-				Accessibility Project
-			</h1>
-			<nav className="flex flex-row gap-4 pt-2">
-				{/* <Link
-					className="border-b-4 border-black hover:border-white focus:border-white"
-					href="/"
+		<header>
+			<div className="flex flex-row sm:flex-col p-4 sm:p-8 justify-between">
+				<h1 className="flex flex-row font-bold text-3xl">
+					Accessibility Project
+				</h1>
+				<button
+					className="flex sm:hidden flex-col gap-1 justify-center"
+					onClick={handleClick}
 				>
-					Home
-					hover:border-opacity-100 focus:border-opacity-100
-					border-transparent hover:border-bottom focus:border-bottom focus:outline-none transition-colors duration-300
-				</Link> */}
-				{/* <Link
-					href="/"
-					className="border-b-4 border-transparent hover:border-opacity-100 focus:border-opacity-100 focus:outline-none transition-opacity duration-300"
-					style={{
-						borderColor: `rgb(var(--header-underline))`,
-					}}
-				>
-					Home
-				</Link> */}
-				<Link href="/" className="headerLink">
+					<div className="h-1 w-8 bg-white"></div>
+					<div className="h-1 w-8 bg-white"></div>
+					<div className="h-1 w-8 bg-white"></div>
+				</button>
+			</div>
+			<nav
+				className={`${
+					showMenu ? "flex" : "hidden"
+				} sm:flex flex-col sm:flex-row gap-4 pt-2`}
+			>
+				<Link onClick={handleClick} href="/" className="headerLink">
 					Home
 				</Link>
-				<Link href="/percievable" className="headerLink">
+				<Link
+					onClick={handleClick}
+					href="/percievable"
+					className="headerLink"
+				>
 					Percievable
 				</Link>
-				<Link className="headerLink" href="/operable">
+				<Link
+					onClick={handleClick}
+					className="headerLink"
+					href="/operable"
+				>
 					Operable
 				</Link>
-				<Link className="headerLink" href="/understandable">
+				<Link
+					onClick={handleClick}
+					className="headerLink"
+					href="/understandable"
+				>
 					Understandable
 				</Link>
-				<Link className="headerLink" href="/robust">
+				<Link
+					onClick={handleClick}
+					className="headerLink"
+					href="/robust"
+				>
 					Robust
 				</Link>
 			</nav>
